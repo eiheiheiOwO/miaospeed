fa31e884b0378e539bb903cdff5c8ec98912b081
aipeach <44671411+aipeach@users.noreply.github.com>
2024-05-27 13:04:21 +0800

    update

--- docker/install.sh
+++ docker/install.sh
@@ -8,38 +8,23 @@ echo "平台: ${ARCH}"
 # 获取系统位数
 BITS=$(getconf LONG_BIT)
 
-# 使用 readelf 获取 ELF 标头信息
-if [ "$ARCH" = "armv7l" ]; then
-  ARM_VERSION=$(readelf -A /proc/self/exe | grep Tag_CPU_arch: | awk '{print $2}')
-  echo "ARM版本: $ARM_VERSION"
-fi
-
 # 获取最新的标签名称
 LATEST_TAG=$(curl -s https://api.github.com/repos/AirportR/miaospeed/releases/latest | grep 'tag_name' | cut -d '"' -f 4)
 
 if [ "$ARCH" = "x86_64" ] && [ "$BITS" = "64" ]; then
   echo "架构: linux/amd64"
-  wget -O /opt/miaospeed.gz https://github.com/AirportR/miaospeed/releases/download/"$LATEST_TAG"/miaospeed-linux-amd64-"$LATEST_TAG".gz
-  gzip -d /opt/miaospeed.gz
-  chmod +x /opt/miaospeed
+  ARCH="linux-amd64"
 elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
   echo "架构: linux/arm64"
-  wget -O /opt/miaospeed.gz https://github.com/AirportR/miaospeed/releases/download/"$LATEST_TAG"/miaospeed-linux-arm64-"$LATEST_TAG".gz
-  gzip -d /opt/miaospeed.gz
-  chmod +x /opt/miaospeed
-elif [ "$ARCH" = "armv7l" ] && [ "$ARM_VERSION" = "v6KZ" ]; then
-  echo "架构: linux/arm/v6"
-  wget -O /opt/miaospeed.gz https://github.com/AirportR/miaospeed/releases/download/"$LATEST_TAG"/miaospeed-linux-armv6-"$LATEST_TAG".gz
-  gzip -d /opt/miaospeed.gz
-  chmod +x /opt/miaospeed
-elif [ "$ARCH" = "armv7l" ] && [ "$ARM_VERSION" = "v7" ]; then
+  ARCH="linux-arm64"
+elif [ "$ARCH" = "armv7l" ]; then
   echo "架构: linux/arm/v7"
-  wget -O /opt/miaospeed.gz https://github.com/AirportR/miaospeed/releases/download/"$LATEST_TAG"/miaospeed-linux-armv7-"$LATEST_TAG".gz
-  gzip -d /opt/miaospeed.gz
-  chmod +x /opt/miaospeed
+  ARCH="linux-armv7"
 elif [ "$ARCH" = "x86_64" ] && [ "$BITS" = "32" ]; then
   echo "架构: linux/386"
-  wget -O /opt/miaospeed.gz https://github.com/AirportR/miaospeed/releases/download/"$LATEST_TAG"/miaospeed-linux-386-"$LATEST_TAG".gz
-  gzip -d /opt/miaospeed.gz
-  chmod +x /opt/miaospeed
+  ARCH="linux-386"
 fi
+
+curl -L "https://github.com/AirportR/miaospeed/releases/download/$LATEST_TAG/miaospeed-$ARCH-$LATEST_TAG.gz" -o "/opt/miaospeed.gz"
+gzip -d /opt/miaospeed.gz
+chmod +x /opt/miaospeed
