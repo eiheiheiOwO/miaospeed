0acffbed466d21dc1a8a7fa0c8879aa62125bdbb
AirportR <AirportRoster@gmail.com>
2024-08-07 20:46:10 +0800

    :memo: Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -27,4 +27,5 @@ fi
 
 curl -L "https://github.com/AirportR/miaospeed/releases/download/$LATEST_TAG/miaospeed-$ARCH-$LATEST_TAG.tar.gz" -o "/opt/miaospeed.tar.gz"
 tar -xzf /opt/miaospeed.tar.gz
+mv /opt/miaospeed-$ARCH-"$LATEST_TAG" /opt/miaospeed
 chmod +x /opt/miaospeed
